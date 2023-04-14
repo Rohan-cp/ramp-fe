31971a1aa8a648d8edfc4d5fc694008b78250759
Rohan-cp <rohancp9@icloud.com>
2023-04-13 21:40:02 -0400

    issue #5 fixed

--- src/App.tsx
+++ src/App.tsx
@@ -22,11 +22,10 @@ export function App() {
   const loadAllTransactions = useCallback(async () => {
     setIsLoading(true)
     transactionsByEmployeeUtils.invalidateData()
-
     await employeeUtils.fetchAll()
+    setIsLoading(false)
     await paginatedTransactionsUtils.fetchAll()
 
-    setIsLoading(false)
   }, [employeeUtils, paginatedTransactionsUtils, transactionsByEmployeeUtils])
 
   const loadTransactionsByEmployee = useCallback(
